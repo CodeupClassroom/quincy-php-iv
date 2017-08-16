e2eb09fe08a4c62d51f14369e9e01d7de56a9fd2
Justin Reich <justinreich.dev@gmail.com>
2017-08-16 11:45:28 -0500

    Add solution for overriding lesson

--- Square.php
+++ Square.php
@@ -4,6 +4,13 @@ require_once "Rectangle.php";
 
 class Square extends Rectangle
 {
+
+	public function __construct($height)
+	{
+		$width = $height;
+		parent::__construct($height, $width);
+	}
+
     public function perimeter()
     {
         return $this->width * 2 + $this->height * 2;
